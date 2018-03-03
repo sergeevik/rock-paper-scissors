bfb80b451e610762b1b6ac2989925b1dcfc0e84b
Viktor <vampirit@mail.ru>
2018-03-03 20:55:25 +0400

    score logic

--- src/main/java/game/entity/user/User.java
+++ src/main/java/game/entity/user/User.java
@@ -9,10 +9,31 @@ import lombok.experimental.Delegate;
 public class User implements Fight<User> {
     @Setter
     private Hand hand;
-    @Delegate(types = CounterImpl.class)
+    @Delegate(types = CounterImpl.class, excludes = SetScore.class)
     private Counter statistic = new CounterImpl();
 
     public Result fight(User anotherUser) {
-        return hand.fight(anotherUser.hand);
+        Result fight = hand.fight(anotherUser.hand);
+        updateMyScore(fight);
+        updateAnotherScore(fight, anotherUser);
+        return fight;
+    }
+
+    private void updateAnotherScore(Result fight, User user) {
+        updateScore(fight, user);
+    }
+
+    private void updateMyScore(Result fight) {
+        updateScore(fight, this);
+    }
+
+    private void updateScore(Result result, User user) {
+        if (result == Result.WIN) {
+            statistic.win();
+        } else if (result == Result.LOSE) {
+            statistic.lose();
+        } else if (result == Result.DRAW) {
+            statistic.draw();
+        }
     }
 }
