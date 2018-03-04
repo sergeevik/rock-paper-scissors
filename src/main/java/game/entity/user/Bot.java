40982e9d8c31b117ff2082a42d1eed1387daef99
Viktor <vampirit@mail.ru>
2018-03-05 00:53:25 +0400

    (int)System.currentTimeMillis() sometimes return negative value. Replace with Random.class

--- src/main/java/game/entity/user/Bot.java
+++ src/main/java/game/entity/user/Bot.java
@@ -1,16 +1,21 @@
 package game.entity.user;
 
 import game.entity.Result;
-import game.entity.hand.*;
+import game.entity.hand.Hand;
+import game.entity.hand.HandForm;
+
+import java.util.Random;
 
 public class Bot extends User {
+    private final Random random = new Random();
+
     public Bot() {
         super("Bot");
     }
 
     private Hand randomHand() {
-        int random = (int)System.currentTimeMillis() % 3;
-        return HandForm.getHandById(random);
+        int value = random.nextInt(3);
+        return HandForm.getHandById(value);
     }
 
     @Override
